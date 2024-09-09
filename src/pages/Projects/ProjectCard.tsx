d75c99480e413b979ed6194f9cc174119aa576a8
Nicolas <nicolasgp.ec@gmail.com>
2024-09-09 14:44:14 -0300

    Add formatting for mobile responsiveness

--- src/pages/Projects/ProjectCard.tsx
+++ src/pages/Projects/ProjectCard.tsx
@@ -94,6 +94,10 @@ const TitleWrapper = styled.div`
   justify-content: space-between;
   width: 100%;
   margin: 0 1rem;
+
+  @media screen and (max-width:768px) {
+    margin: 0 1rem 1rem 1rem;
+  }
 `
 
 const InfoWrapper = styled.div`
